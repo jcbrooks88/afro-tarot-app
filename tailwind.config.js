83a7914e92b0a1b2cf46449d891645169625959f
UNC Coding Bootcamp <UNCBootcamp@Jefferys-MacBook.local>
2025-07-05 14:15:28 -0400

    tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,4 +3,18 @@ content: [
     "./components/**/*.{js,ts,jsx,tsx}",
     "./app/**/*.{js,ts,jsx,tsx}",
   ]
+
+  theme: {
+    extend: {
+      animation: {
+        fadeIn: 'fadeIn 0.3s ease-in-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: 0 },
+          '100%': { opacity: 1 },
+        },
+      },
+    },
+  },
   
