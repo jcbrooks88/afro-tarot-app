f12dd4e9fc208daea413918c76ed7055b0dea3c2
UNC Coding Bootcamp <UNCBootcamp@Jefferys-MacBook.local>
2025-07-14 18:24:45 -0400

    moon.tsx function update

--- src/pages/Moon.tsx
+++ src/pages/Moon.tsx
@@ -1,6 +1,6 @@
 import MonthlyMoonCalendar from "../components/moon/Calendar";
 
-export default function MoonCalendarPage() {
+const MoonCalendarPage: React.FC = () => {
   return (
     <main className="p-6 max-w-6xl mx-auto">
       <h1 className="text-3xl font-bold mb-4">Moon Calendar</h1>
@@ -11,5 +11,7 @@ export default function MoonCalendarPage() {
     </main>
   );
 }
+
+export default MoonCalendarPage
 
 
