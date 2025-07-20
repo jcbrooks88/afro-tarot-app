b6ddd796b1bfd7e52f93cbe2c57900daf0670c82
UNC Coding Bootcamp <UNCBootcamp@Jefferys-MacBook.local>
2025-07-20 14:58:19 -0400

    Orisha homepage summary

--- src/pages/orisha.tsx
+++ src/pages/orisha.tsx
@@ -15,7 +15,7 @@ const OrishaPage: React.FC = () => {
           Connect to Your Inner Orisha
         </h1>
         <p className="text-gray-700 font-semibold dark:text-gray-300 max-w-2xl mx-auto">
-          Dive into each Orisha&apos;s symbolism & meaning.
+          Orisha&apos;s are spiritual beings in the Yoruba religion, each with unique qualities that connect them to different aspects of life. Similarly, the Zodiac and planets are systems with symbolic meanings tied to personality, fate, and the cosmos. Exploring the connections between Orisha&apos;s, Zodiac signs, and planetary influences can deepen your spiritual understanding. However, these associations vary across traditions, regions, and lineages. It&apos;s important to stay open-minded, research thoroughly, and build a personal connection that feels authentic to your path.
         </p>
       </motion.div>
 
